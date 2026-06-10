5c923fe5a73283518a8e269522e37abdaf2c72e8
ltippett1 <liam.tippett@students.plymouth.ac.uk>
2026-06-10 13:13:48 +0100

    Added bird movement, score system and fixed memory usage for pipe array. Still need to fix the score system

--- code/summer.js
+++ code/summer.js
@@ -31,6 +31,11 @@ let bottom_pipe_img;
 
 //game physics
 let velocity_x = -2; //pipe moving left speed
+let velocity_y = 0; //bird jump speed
+let gravity = 0.4; //bring bird down
+
+let game_over = false;
+let score = 0;
 
 window.onload = function() {
     board = document.getElementById("board");
@@ -55,27 +60,59 @@ window.onload = function() {
 
     requestAnimationFrame(update);
     setInterval(place_pipes, 1500); //every 1.5 seconds
+    document.addEventListener("keydown", move_bird);
 }
 
 function update() {
     requestAnimationFrame(update);
+    if (game_over) {
+        return;
+    }
     context.clearRect(0, 0, board_width, board_height);
 
     //bird
+    velocity_y += gravity;
+    bird_y = Math.max(bird_y + velocity_y, 0); //apply gravity to current bird_y, limit the bird_y to the top of the canvas
+    
+    bird.y = bird_y;
+    
     context.drawImage(bird_image, bird_x, bird_y, bird_width, bird_height);
+
+    if (bird_y > board_height) {
+        game_over = true;
+    }
 
     //pipes
     for (let i = 0; i < pipe_array.length; i++) {
         let pipe = pipe_array[i];
         pipe.x += velocity_x;
         context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+    
+        if (!pipe.passed && bird.x > pipe.x + pipe_width) {
+            score += 0.5; //0.5 because there are 2 pipes.
+            pipe.passed = true;
+        }
+
+        if (detect_collision(bird, pipe)) {
+            game_over = true;
+        }
     }
 
+    //clear pipes
+    while (pipe_array.length > 0 && pipe_array[0].x < 0) {
+        pipe_array.shift(); //removes first element from array
+    }
 
-
+    //score
+    context.fillStyle = "white";
+    context.font = "45px sans-serif";
+    context.fillText(score, 5, 45);
 }
 
 function place_pipes() {
+    if (game_over) {
+        return;
+    }
 
     //(0-1) * pipe_height/2
     // 0 -> -128 (pipe_height/4)
@@ -104,4 +141,18 @@ function place_pipes() {
     }
     
     pipe_array.push(bottom_pipe);
+}
+
+function move_bird(e) {
+    if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX") {
+        //jump
+        velocity_y = -6;
+    }
+}
+
+function detect_collision (a, b) {
+    return  a.x < b.x + b.width &&
+            a.x + a.width > b.x &&
+            a.y < b.y + b.height &&
+            a.y + a.height > b.y
 }
